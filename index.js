28f0704b2187df74f77060c21fc6ca7a1d4784ea
het0079 <hetsolankipersonal@gmail.com>
2026-02-14 15:17:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const express = require("express");
-require("dotenv").config();   // MUST be here before using process.env
-const mongoose = require("mongoose");
-const cors = require("cors");
-const app = express();
+'use strict';
 
-app.use(cors());
-app.use(express.json());
+var implementation = require('./implementation');
 
-// Routes
-const userRoutes = require("../routes/usersRoute");
-const carRoutes = require("../routes/carsRoute");
-const bookingRoutes = require("../routes/bookingsRoute");
-
-app.use("/api/users", userRoutes);
-app.use("/api/cars", carRoutes);
-app.use("/api/bookings", bookingRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+module.exports = Function.prototype.bind || implementation;
